ce21f82f5bc1c59bb60dcabe748676f8e83eebcd
Cam D. Coleman <107205722+kamu-49@users.noreply.github.com>
2023-04-20 15:18:30 -0400

    transition

--- gobackn.py
+++ gobackn.py
@@ -25,10 +25,34 @@ holder = 0
 SENDER
 """
 def sender(self_port, peer_port, window_size, type, value):
+    #declarations
     sport = self_port
     rport = peer_port
     ws = window_size
-    pass
+    saddr = ('', sport)
+    raddr = ('', rport)
+    wsc = 0 #window size counter
+
+    #socket init
+    sock = socket(AF_INET, SOCK_DGRAM)
+    socket.bind(saddr)
+
+    #buffer init
+    buffer = [data_packet_size*ws]
+
+    #message init
+    msg = input("sender> ")
+    #testing correct user input
+    while(msg.split()[0] != "send"):
+        msg = input("incorrect pharasing. supposed to be 'send (insert text here)'. try again please\nsender> ")
+    letter_list = [char for char in msg]
+    
+    #im not sure
+    while True:
+        while wsc < ws:
+            
+
+
 
 
 
